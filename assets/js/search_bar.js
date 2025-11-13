12df7ac38d96ac8977b3c8687696f1774f6a6278
Aidanng19 <aidanstriker13@gmail.com>
2025-11-12 22:36:07 -0500

    List works perfectly

--- assets/js/search_bar.js
+++ assets/js/search_bar.js
@@ -1,6 +1,6 @@
   //Define local variables
   const searchInput = document.getElementById("search_input");
-  const myList = document.getElementById('list');
+  const myList = document.getElementById('searchbarlist');
 
   // if an element doesn't match the input letter, filter it out
   searchInput.addEventListener('input', ()=>{
